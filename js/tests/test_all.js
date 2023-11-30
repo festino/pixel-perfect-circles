19afb78d06e2a813ec3c19d40b1c7c8dd0d1c3e1
Festino <offestashka@mail.ru>
2023-12-01 01:30:45 +0300

    test(page): add isPossibleLengths tests

--- js/tests/test_all.js
+++ js/tests/test_all.js
@@ -18,3 +18,31 @@ function test_getDefaultLengths() {
 	}
 	return testStatus;
 }
+
+function test_isPossibleLengths() {
+	let testStatus = true;
+	for (let size = 1; size <= 30; size++) {
+		let lengthsOptions = getLengthOptions(size, false);
+		for (const lengths of lengthsOptions) {
+			if (!isPossibleLengths(Math.floor((size + 1) / 2), lengths)) {
+				console.log("x test_isPossibleLengths (" + size + "): " + lengths);
+				testStatus = false;
+			}
+		}
+	}
+	return testStatus;
+}
+
+function test_isPossibleLengths_monotonous() {
+	let testStatus = true;
+	for (let size = 1; size <= 30; size++) {
+		let lengthsOptions = getLengthOptions(size, true);
+		for (const lengths of lengthsOptions) {
+			if (!isPossibleLengths(Math.floor((size + 1) / 2), lengths)) {
+				console.log("x test_isPossibleLengths (" + size + "): " + lengths);
+				testStatus = false;
+			}
+		}
+	}
+	return testStatus;
+}
